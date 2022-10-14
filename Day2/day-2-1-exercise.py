a44df21333b402a5a65dd33b0c827ef16496436b
Mohamed Amine lakbachi <83095379+amine2241@users.noreply.github.com>
2022-10-14 17:16:50 +0100

    Create day-2-1-exercise.py

---
+++ Day2/day-2-1-exercise.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+new_two_digit_number=str(two_digit_number)
+print(int(new_two_digit_number [0]) + int(new_two_digit_number [1]))
