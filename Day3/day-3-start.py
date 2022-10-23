05709b24aaf04d0f7f68b0f87bee508ba9837124
Mohamed Amine lakbachi <83095379+amine2241@users.noreply.github.com>
2022-10-23 14:23:24 +0100

    Create day-3-start.py

---
+++ Day3/day-3-start.py
@@ -0,0 +1,13 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height != 120:
+  print("You can ride the rollercoaster ")
+  age=int(input("what is your age"))
+  if age<12 :
+    print("please pay $5")
+  elif age<=18:
+    print("please pay 7$")
+  else:
+    print("Please pay $12.")
+else:
+  print("you cannot ride the rollercoaste")
